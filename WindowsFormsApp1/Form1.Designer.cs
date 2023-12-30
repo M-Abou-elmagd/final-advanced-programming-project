ed7ec6c5b13cb66f446b89fbc231d3a9da49bc03
M-Abou-elmagd <155028418+M-Abou-elmagd@users.noreply.github.com>
2023-12-30 19:10:04 +0200

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace BlackjackGame
+{
+    partial class Form1
+    {
+        private System.Windows.Forms.Label playerCardsLabel;
+        private System.Windows.Forms.Label dealerCardsLabel;
+        private System.Windows.Forms.Label playerScoreLabel;
+        private System.Windows.Forms.Label dealerScoreLabel;
+        private System.Windows.Forms.Button HitButton;
+        private System.Windows.Forms.Button StandButton;
+
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.playerCardsLabel = new System.Windows.Forms.Label();
+            this.dealerCardsLabel = new System.Windows.Forms.Label();
+            this.HitButton = new System.Windows.Forms.Button();
+            this.StandButton = new System.Windows.Forms.Button();
+            this.playerScoreLabel = new System.Windows.Forms.Label();
+            this.dealerScoreLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // playerCardsLabel
+            // 
+            this.playerCardsLabel.BackColor = System.Drawing.Color.Transparent;
+            this.playerCardsLabel.Font = new System.Drawing.Font("Mistral", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playerCardsLabel.ForeColor = System.Drawing.Color.Red;
+            this.playerCardsLabel.Location = new System.Drawing.Point(879, 398);
+            this.playerCardsLabel.Name = "playerCardsLabel";
+            this.playerCardsLabel.Size = new System.Drawing.Size(318, 191);
+            this.playerCardsLabel.TabIndex = 0;
+            // 
+            // dealerCardsLabel
+            // 
+            this.dealerCardsLabel.BackColor = System.Drawing.Color.Transparent;
+            this.dealerCardsLabel.Font = new System.Drawing.Font("Mistral", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dealerCardsLabel.ForeColor = System.Drawing.Color.Red;
+            this.dealerCardsLabel.Location = new System.Drawing.Point(879, 689);
+            this.dealerCardsLabel.Name = "dealerCardsLabel";
+            this.dealerCardsLabel.Size = new System.Drawing.Size(289, 199);
+            this.dealerCardsLabel.TabIndex = 1;
+            // 
+            // HitButton
+            // 
+            this.HitButton.Location = new System.Drawing.Point(887, 948);
+            this.HitButton.Name = "HitButton";
+            this.HitButton.Size = new System.Drawing.Size(176, 73);
+            this.HitButton.TabIndex = 2;
+            this.HitButton.Text = "Hit";
+            this.HitButton.Click += new System.EventHandler(this.HitButton_Click);
+            // 
+            // StandButton
+            // 
+            this.StandButton.Location = new System.Drawing.Point(1115, 948);
+            this.StandButton.Name = "StandButton";
+            this.StandButton.Size = new System.Drawing.Size(177, 73);
+            this.StandButton.TabIndex = 3;
+            this.StandButton.Text = "Stand";
+            this.StandButton.Click += new System.EventHandler(this.StandButton_Click);
+            // 
+            // playerScoreLabel
+            // 
+            this.playerScoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.playerScoreLabel.Font = new System.Drawing.Font("Mistral", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playerScoreLabel.ForeColor = System.Drawing.Color.Red;
+            this.playerScoreLabel.Location = new System.Drawing.Point(1433, 389);
+            this.playerScoreLabel.Name = "playerScoreLabel";
+            this.playerScoreLabel.Size = new System.Drawing.Size(300, 229);
+            this.playerScoreLabel.TabIndex = 4;
+            this.playerScoreLabel.Click += new System.EventHandler(this.playerScoreLabel_Click);
+            // 
+            // dealerScoreLabel
+            // 
+            this.dealerScoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.dealerScoreLabel.Font = new System.Drawing.Font("Mistral", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dealerScoreLabel.ForeColor = System.Drawing.Color.Red;
+            this.dealerScoreLabel.Location = new System.Drawing.Point(1418, 709);
+            this.dealerScoreLabel.Name = "dealerScoreLabel";
+            this.dealerScoreLabel.Size = new System.Drawing.Size(440, 273);
+            this.dealerScoreLabel.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1913, 1151);
+            this.Controls.Add(this.playerCardsLabel);
+            this.Controls.Add(this.dealerCardsLabel);
+            this.Controls.Add(this.HitButton);
+            this.Controls.Add(this.StandButton);
+            this.Controls.Add(this.playerScoreLabel);
+            this.Controls.Add(this.dealerScoreLabel);
+            this.Name = "Form1";
+            this.Text = "BlackJack Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        // Rest of auto-generated code
+        // ...
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
+namespace BlackjackGame
+{
+    public partial class Form1 : Form
+    {
+        private Stack<string> deck = new Stack<string>();
+        private List<string> playerHand = new List<string>();
+        private List<string> dealerHand = new List<string>();
+        private Random rand = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeDeck();
+            DealInitialCards();
+        }
+
+        private void InitializeDeck()
+        {
+            string[] suits = { "Hearts", "Diamonds", "Clubs", "Spades" };
+            string[] values = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+
+            foreach (string suit in suits)
+            {
+                foreach (string value in values)
+                {
+                    deck.Push(value + " of " + suit);
+                }
+            }
+
+            ShuffleDeck();
+        }
+
+        private void ShuffleDeck()
+        {
+            var shuffledDeck = deck.ToArray();
+            int n = shuffledDeck.Length;
+            while (n > 1)
+            {
+                n--;
+                int k = rand.Next(n + 1);
+                var value = shuffledDeck[k];
+                shuffledDeck[k] = shuffledDeck[n];
+                shuffledDeck[n] = value;
+            }
+            deck = new Stack<string>(shuffledDeck);
+        }
+
+        private void DealInitialCards()
+        {
+            playerHand.Add(DealCard());
+            dealerHand.Add(DealCard());
+            playerHand.Add(DealCard());
+            dealerHand.Add(DealCard());
+
+            UpdateUI();
+        }
+
+        private string DealCard()
+        {
+            if (deck.Count > 0)
+                return deck.Pop();
+            else
+                throw new InvalidOperationException("Deck is empty.");
+        }
+
+        private void UpdateUI()
+        {
+            playerCardsLabel.Text = string.Join("\n", playerHand);
+            playerScoreLabel.Text = "Score: " + CalculateScore(playerHand);
+
+            dealerCardsLabel.Text = string.Join("\n", dealerHand);
+            dealerScoreLabel.Text = "Score: " + CalculateScore(dealerHand);
+        }
+
+        private int CalculateScore(List<string> hand)
+        {
+            int score = 0;
+            int aceCount = 0;
+
+            foreach (string card in hand)
+            {
+                string value = card.Split(' ')[0];
+                if (value == "A")
+                {
+                    aceCount++;
+                    score += 11;
+                }
+                else if (value == "K" || value == "Q" || value == "J")
+                {
+                    score += 10;
+                }
+                else
+                {
+                    score += int.Parse(value);
+                }
+            }
+
+            while (score > 21 && aceCount > 0)
+            {
+                score -= 10;
+                aceCount--;
+            }
+
+            return score;
+        }
+
+        private void HitButton_Click(object sender, EventArgs e)
+        {
+            playerHand.Add(DealCard());
+            UpdateUI();
+
+            if (CalculateScore(playerHand) > 21)
+            {
+                MessageBox.Show("Player busts! Dealer wins!");
+                EndGame();
+            }
+        }
+
+        private void StandButton_Click(object sender, EventArgs e)
+        {
+            while (CalculateScore(dealerHand) < 17)
+            {
+                dealerHand.Add(DealCard());
+            }
+
+            UpdateUI();
+
+            int playerScore = CalculateScore(playerHand);
+            int dealerScore = CalculateScore(dealerHand);
+
+            if (dealerScore > 21 || playerScore > dealerScore)
+            {
+                MessageBox.Show("Player wins!");
+            }
+            else if (playerScore < dealerScore)
+            {
+                MessageBox.Show("Dealer wins!");
+            }
+            else
+            {
+                MessageBox.Show("It's a tie!");
+            }
+            EndGame();
+        }
+
+        private void EndGame()
+        {
+            HitButton.Enabled = false;
+            StandButton.Enabled = false;
+
+            var result = MessageBox.Show("Do you want to play again?", "Game Over", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                RestartGame();
+            }
+            else
+            {
+                Close();
+            }
+        }
+
+        private void RestartGame()
+        {
+            deck.Clear();
+            playerHand.Clear();
+            dealerHand.Clear();
+
+            InitializeDeck();
+            DealInitialCards();
+
+            HitButton.Enabled = true;
+            StandButton.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Welcome to the Blackjack game!");
+        }
+
+        private void playerScoreLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
